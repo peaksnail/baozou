2a48796ea50b3186f265376b8e6e1feae4ba1e9b
peaksnail <peaksnail@gmail.com>
2013-03-26 20:56:04 +0800

    change version 4

--- baozou.4.sh
+++ baozou.4.sh
@@ -1,7 +1,8 @@
 #-------------
 #  psnil 
-#  2013-3-3
-#  pm 16:00
+#  2013-3-26
+#  pm 20:00
+#  文件目录组织有点差阿,需要锻炼
 #-------------
 #!/bin/bash  
 #set -x #调试
@@ -11,11 +12,20 @@ declare -i count=0
 declare -i exist=0
 declare -a src
 declare -i test=1
-pwd=`pwd`
+filename=`date +"%Y_%m_%d"`.zip
+
+# 判断smtp是否启动
+source test.sh              #用来判断各项服务是否开启了
+smtp
+net
+
+echo "begin collect..."
+
+pwd=`pwd`                   #这些可以自己设置路径
 tmp=/tmp/$$                 #下载urlfile所指路径的网页，下载上面的图片链接
-des=pic                     #下载后存放图片的文件夹
+des=pic/                    #下载后存放图片的文件夹
 urlfile=baozou_url          #存放要下载的暴走图片所在网站的url   
-data=baozou_data  #存放已经下载过的暴走 图片的url
+data=baozou_data            #存放已经下载过的暴走 图片的url
 
 #取得url列表
 for url in `cat $urlfile`
@@ -27,7 +37,7 @@ done
 ((count--)) 
 
 if [ ! -f $data ];then
-    touch baozou_data
+    echo "there is no baozou_data,please touch "
 fi
 
 cd $des
@@ -59,20 +69,23 @@ rm $tmp
 	    #打包发送操作
 	    exist=`ls  | wc -l`
 	    if [ $exist -ne 0 ];then
-	    #zip -m pic.zip ./new/*
-	    #declare -i num=25  #kindle一次最多接受25个文件
-	     #while [[ $num -gt 0 ]]
-	     #do 
-	#	 cd ./new/
-	#	 zip -m -q  ../pic.zip `ls -rt | head -1`
-	#	 ((num--))
-	 #    done
-	  #   cd ..
-	  #这里采用convert合成图片为一张pdf来发送
+        echo "find comic !"
+
+        #仅发送50张否则过大（不能超过50M）,so 删除多与图片
+            if [ $exist -gt 50 ];then
+                rm ` ls | tail -$(($exist-50)) `
+            fi
+
+        #里采用convert合成图片为一张pdf来发送
 	    convert * +compress baozou.pdf
-	    zip -m baozou.zip baozou.pdf
-	    echo "kindle" | mutt -s "baozou" 513322938@free.kindle.com -a baozou.zip  #一次性传输不能太大，否则会失败，分多次传输
+	    zip -m $filename baozou.pdf
+        echo -n  "mutt now ... wait... the size of file is :"
+        echo `du -h $filename`
+	    echo "kindle" | mutt -s "baozou" XXXXXXXXXX@free.kindle.com -a $filename  
+        echo "mutt ok!"
 	    rm *
-	    cd ..
-    	    fi
+	    cd $pwd
+        else
+            echo "there is no comic !"
+   	    fi
 exit
---
+++ test.sh
@@ -0,0 +1,27 @@
+# @file smtp.sh
+# @Synopsis   判断网络，smtp服务是否已经start
+# @author psnail  peaksnail@gmail.com
+# @version 1
+# @date 2013-03-26
+#!/bin/bash
+
+ smtp ()  {
+declare -i on=0;
+on=`netstat -tl | grep smtp | wc -l`
+if [ $on -eq 0 ];then
+    echo "smtp stop now ,you can openn it with systemctl!"
+    exit
+else
+    echo "smtp have start ..."
+fi
+}
+ net()  {
+     declare -i on=0
+     on=`ifconfig | grep 222.25.181.12 | wc -l` 
+     if [ $on -eq 0 ];then
+        echo "network is down now"
+        exit
+     else 
+        echo "network ready ..."
+     fi
+ }
